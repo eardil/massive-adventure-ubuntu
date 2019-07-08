f4176af86f44733c55ffcad8380c89163d31b618
Irving <irving.simonin@revenatium.com>
2019-07-08 14:10:04 -0500

    added gnome shell theme

--- i_theme_eardil.sh
+++ i_theme_eardil.sh
@@ -9,3 +9,25 @@ cp source-code-pro-1.017R/OTF/*.otf ~/.fonts
 fc-cache -f -v
 ## Cambio de tema oh my zsh
 sed -i -e 's/robbyrussellabc/bureau/g' ~/.zshrc
+
+# gnome theme
+sudo apt-get install gnome-shell
+sudo apt-get install ubuntu-gnome-desktop
+sudo apt-get install ubuntu-gnome-desktop
+sudo update-alternatives --config gdm3.css
+sudo apt-get install chrome-gnome-shell
+sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo apt-get update
+sudo apt-get install flat-remix-gtk
+sudo add-apt-repository universe
+sudo apt install gnome-tweak-tool
+sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo apt-get update
+sudo apt-get install flat-remix
+sudo apt-get install flat-remix-gnome
+sudo apt install php php-common gcc
+sudo apt install libglib2.0-dev-bin imagemagick
+cd ~/git
+git clone https://github.com/daniruiz/flat-remix-gnome
+cd flat-remix-gnome
+make && sudo make install
--- main_eardil.sh
+++ main_eardil.sh
@@ -1,9 +1,8 @@
 ## File that loads everything else, in modules.
 ./i_oh_my_zsh.sh
-./i_R_compiled
 ./i_theme_eardil.sh
 ./i_sublime
-./pyenv
+./i_pyenv
 pyenv install 3.6.0
 pip install ipython
 
